e4c71c0be19014bd5154b486981f6ecd4c91b62e
ritazagoni <zagoni.rita@gmail.com>
2018-10-26 09:23:59 +0300

    Tag flows with info on source

--- concat_flows/concat_flows.py
+++ concat_flows/concat_flows.py
@@ -6,18 +6,18 @@ from core_data_modules.traced_data import TracedData, Metadata
 from core_data_modules.traced_data.io import TracedDataJsonIO
 
 
-def normalise_pdm_flow_keys(pdm_traced_data, key_to_normalise, normalised_key):
+def normalise_activation_flow_keys(activation_td, key_to_normalise, normalised_key):
     """
     Normalises the keys from different PDMs(Post Distribuiton Monitoring survey)
-    Acts on the TracedData Object itself(pdm_traced_data)
-    :param pdm_traced_data: Data to normalise the keys of
-    :type pdm_traced_data: iterable of TracedData
+    Acts on the TracedData Object itself(activation_td)
+    :param activation_td: Data to normalise the keys of
+    :type activation_td: iterable of TracedData
     :param key_to_normalise: Key to search for and normalise
     :type key_to_normalise: str
     :param normalised_key: String create new key from 
     :type normalised_key: str
     """
-    for record in pdm_traced_data:
+    for record in activation_td:
         data_to_append = {}
         for key in record.keys():
             if key_to_normalise in key:
@@ -27,35 +27,45 @@ def normalise_pdm_flow_keys(pdm_traced_data, key_to_normalise, normalised_key):
         record.append_data(data_to_append, md)
 
 
+def set_source_flow(activation_td, source_flow):
+    for record in activation_td:
+        source_flow_data = {"source_flow": source_flow}
+        record.append_data(source_flow_data, Metadata(user, Metadata.get_call_location(), time.time()))
+
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Loads the PDM surveys and concatenates them")
+    parser = argparse.ArgumentParser(description="Loads the activation surveys and concatenates them")
     parser.add_argument("user", help="Identifier of user launching this program, for use in TracedData Metadata")
-    parser.add_argument("pdm1_input_path", help="Path to the first PDM survey")
-    parser.add_argument("pdm2_input_path", help="Path to the second PDM survey")
+    parser.add_argument("radio_activation_input_path", help="Path to the radio activation survey")
+    parser.add_argument("offline_activation_input_path", help="Path to the offline activation survey")
     parser.add_argument("traced_json_output_path", help="Path to concatenated TraceData JSON")
  
     args = parser.parse_args()
     user = args.user
-    pdm1_input_path = args.pdm1_input_path
-    pdm2_input_path = args.pdm2_input_path
+    radio_activation_input_path = args.radio_activation_input_path
+    offline_activation_input_path = args.offline_activation_input_path
     traced_json_output_path = args.traced_json_output_path
 
-    # load the 2 flows that were saved as JSON
-    with open(pdm1_input_path, 'r') as f:
-        traced_pdm1 = TracedDataJsonIO.import_json_to_traced_data_iterable(f)
-    with open(pdm2_input_path, 'r') as f:
-        traced_pdm2 = TracedDataJsonIO.import_json_to_traced_data_iterable(f)
+    # Load the 2 flows that were saved as JSON
+    with open(radio_activation_input_path, 'r') as f:
+        radio_activation_td = TracedDataJsonIO.import_json_to_traced_data_iterable(f)
+    with open(offline_activation_input_path, 'r') as f:
+        offline_activation_td = TracedDataJsonIO.import_json_to_traced_data_iterable(f)
+
+    # Set source flow (radio or offline)
+    set_source_flow(radio_activation_td, 'radio')
+    set_source_flow(offline_activation_td, 'offline')
     
-    # normalise the keys
-    normalise_pdm_flow_keys(traced_pdm1, 'mcf_activation_offline', 'mcf_activation')
-    normalise_pdm_flow_keys(traced_pdm2, 'mcf_activation_radio', 'mcf_activation')
+    # Normalise the keys
+    normalise_activation_flow_keys(radio_activation_td, 'mcf_activation_radio', 'mcf_activation')
+    normalise_activation_flow_keys(offline_activation_td, 'mcf_activation_offline', 'mcf_activation')
     
 
-    # concatenate the PDMs
-    pdm_combined = []
-    pdm_combined.extend(traced_pdm1)
-    pdm_combined.extend(traced_pdm2)
+    # Concatenate the flows
+    activation_combined = []
+    activation_combined.extend(radio_activation_td)
+    activation_combined.extend(offline_activation_td)
     
 
     with open(traced_json_output_path, "w") as f:
-        TracedDataJsonIO.export_traced_data_iterable_to_json(pdm_combined, f, pretty_print=True)
+        TracedDataJsonIO.export_traced_data_iterable_to_json(activation_combined, f, pretty_print=True)
